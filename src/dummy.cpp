fba3c913c426b9c7b73d0bc989970ec9bf2e7c41
recipelab <recipelab17@gmail.com>
2023-12-05 00:08:58 +0900

    debug
    1. 더미 콜백수정

--- src/dummy.cpp
+++ src/dummy.cpp
@@ -7,28 +7,36 @@ std::string id;
 bool panelOutCallback(faduino::PanelOut::Request &req, faduino::PanelOut::Response &res) {
     ROS_INFO("[%s] %s", id.c_str(), __FUNCTION__);
     if (req.buzzer.update) {
-        printf("buzzer updated.\n");
+        printf("buzzer updated[cnt:%d, on:%d, off:%d, last:%d, order:%d].\n",
+            req.buzzer.targetCount, req.buzzer.onTime, req.buzzer.offTime, req.buzzer.lastState, req.buzzer.order);
     }
     if (req.md_power.update) {
-        printf("md_power updated.\n");
+        printf("md_power updated[cnt:%d, on:%d, off:%d, last:%d, order:%d].\n",
+            req.md_power.targetCount, req.md_power.onTime, req.md_power.offTime, req.md_power.lastState, req.md_power.order);
     }
     if (req.md_estop.update) {
-        printf("md_estop updated.\n");
+        printf("md_estop updated[cnt:%d, on:%d, off:%d, last:%d, order:%d].\n",
+            req.md_estop.targetCount, req.md_estop.onTime, req.md_estop.offTime, req.md_estop.lastState, req.md_estop.order);
     }
     if (req.led_start.update) {
-        printf("led_start updated.\n");
+        printf("led_start updated[cnt:%d, on:%d, off:%d, last:%d, order:%d].\n",
+            req.led_start.targetCount, req.led_start.onTime, req.led_start.offTime, req.led_start.lastState, req.led_start.order);
     }
     if (req.led_stop.update) {
-        printf("led_stop updated.\n");
+        printf("led_stop updated[cnt:%d, on:%d, off:%d, last:%d, order:%d].\n",
+            req.led_stop.targetCount, req.led_stop.onTime, req.led_stop.offTime, req.led_stop.lastState, req.led_stop.order);
     }
     if (req.bat_relay.update) {
-        printf("bat_relay updated.\n");
+        printf("bat_relay updated[cnt:%d, on:%d, off:%d, last:%d, order:%d].\n",
+            req.bat_relay.targetCount, req.bat_relay.onTime, req.bat_relay.offTime, req.bat_relay.lastState, req.bat_relay.order);
     }
     if (req.out_spare1.update) {
-        printf("out_spare1 updated.\n");
+        printf("out_spare1 updated[cnt:%d, on:%d, off:%d, last:%d, order:%d].\n",
+            req.out_spare1.targetCount, req.out_spare1.onTime, req.out_spare1.offTime, req.out_spare1.lastState, req.out_spare1.order);
     }
     if (req.out_spare2.update) {
-        printf("out_spare2 updated.\n");
+        printf("out_spare2 updated[cnt:%d, on:%d, off:%d, last:%d, order:%d].\n",
+            req.out_spare2.targetCount, req.out_spare2.onTime, req.out_spare2.offTime, req.out_spare2.lastState, req.out_spare2.order);
     }
     
 #define SRV_SUCCESS	true
